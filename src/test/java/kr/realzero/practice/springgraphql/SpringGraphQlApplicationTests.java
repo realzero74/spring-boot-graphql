54cdf13b6f45a728fd689c855971f6fe7a9bdd03
jinyoung <realzero74@gmail.com>
2022-09-17 00:23:37 +0900

    init commit

---
+++ src/test/java/kr/realzero/practice/springgraphql/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.realzero.practice.springgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
